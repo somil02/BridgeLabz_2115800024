8dbe94cffd4935029a93878fc9c5cbfdaea9bea3
somil02 <somilgupta091@gmail.com>
2025-03-02 22:19:56 +0530

    [Somil Gupta] Add . ORM-EntityFrameworkCore in C#

---
+++ EmployeeManagementProj/BusinessLayer/Interface/IEmployeeBL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Service;
+using CommonLayer;
+
+namespace BusinessLayer.Interface
+{
+    public interface IEmployeeBL
+    {
+        void AddEmployee(Employee employee);
+    }
+}
